da2b0918dc56857b7b3b0327304dadc0a4bc637e
David McGrann <dpmcgrann@gmail.com>
2018-04-17 18:55:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,7 @@
 class Student < User
   attr_accessor :knowledge
   
-  def initialize(knowledge)  
-    user.new = self
-    @knowledge = []
-  end
-  
-  def learn(string)
+   def learn(string)
     @knowledge << string
   end
   
