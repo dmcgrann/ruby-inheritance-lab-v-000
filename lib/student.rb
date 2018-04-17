ef9e62b73bb2bd091bce8c9eafdb6e377dc2eb39
David McGrann <dpmcgrann@gmail.com>
2018-04-17 18:53:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,16 @@
 class Student < User
   
+  def new  
+    user.new = self
+    @knowledge = []
+  end
   
   def learn(string)
     @knowledge << string
   end
   
   def knowledge
-    @knowledge = []
+    @knowledge
   end
   
 end
