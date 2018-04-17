7b5926177f90e554f77e753d0e6c660347346f46
David McGrann <dpmcgrann@gmail.com>
2018-04-17 18:47:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,6 @@
 class Student < User 
   
-  def self.new
-    @first_name = first_name
-    @last_name = last_name
-    @knowledge = []
-  end
+  
   
   def learn(string)
     @knowledge << string
